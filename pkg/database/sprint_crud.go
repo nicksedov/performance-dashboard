815b848b165314bf4e0b2f132036950c766334df
Nikolay Sedov <nicksedov@yandex.ru>
2024-04-30 23:58:05 +0300

    Sprint metadata improvements

--- pkg/database/sprint_crud.go
+++ pkg/database/sprint_crud.go
@@ -23,13 +23,14 @@ func SaveSprint(s *jira.Sprint) error {
 	existing := database.Sprint{}
 	tx := db.Where(database.Sprint{ID: sprint.ID}).First(&existing)
 	if tx.Error == nil {
-		sprint.ID = existing.ID
 		if !existing.Equals(&sprint) {
+			sprint.LastPollID = existing.LastPollID
 			db.Save(&sprint)
 		} else {
 			log.Printf("Sprint with ID '%d' is already known\n", sprint.ID)
 		}
 	} else {
+	    log.Printf("A new sprint '%s' will be created\n", sprint.Name)
 		db.Save(&sprint)
 	}
 
